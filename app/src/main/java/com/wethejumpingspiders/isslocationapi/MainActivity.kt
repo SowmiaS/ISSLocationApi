f04ff3f0b38d1bf7be545c3a1e18a08ae55dcf6d
SowmiaS <sowmiasundar@gmail.com>
2019-06-24 23:59:08 +0530

    refined API structure and implementation

--- app/src/main/java/com/wethejumpingspiders/isslocationapi/MainActivity.kt
+++ app/src/main/java/com/wethejumpingspiders/isslocationapi/MainActivity.kt
@@ -7,13 +7,21 @@ import com.wethejumpingspiders.isslocationapi.locationpointparser.LocationPoint
 import com.wethejumpingspiders.isslocationapi.locationpointparser.LocationPointParser
 import com.wethejumpingspiders.isslocationapi.sightingsparser.ISSSightingDataParser
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , ISSLocator.ISSLocatorInitializationStatus {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ISSLocator(applicationContext).intialise()
+        ISSLocator(applicationContext).intialise(this)
 
+    }
+
+    override fun onSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFailure() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
