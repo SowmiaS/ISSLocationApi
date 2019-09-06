d89c18c33a497c706e0362f4850cff8e2bb68325
SowmiaS <sowmiasundar@gmail.com>
2019-09-06 13:56:57 +0530

    sighting info url error corrected

--- lib/src/main/java/com/wethejumpingspiders/isslocationapi/sightingsparser/SightingRSSService.kt
+++ lib/src/main/java/com/wethejumpingspiders/isslocationapi/sightingsparser/SightingRSSService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 
 interface SightingRSSService {
 
-    @GET("indexrss1.cfm")
+    @GET("indexrss.cfm")
     suspend fun getFeed(@Query("country")  country : String,
                 @Query("region")  region : String,
                 @Query("city")  city : String
